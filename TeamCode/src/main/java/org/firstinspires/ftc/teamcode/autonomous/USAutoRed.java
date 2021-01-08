1e9ca2fc2e39914092ed3a57db9560594eb83c9e
Gent Semaj <gentsemaj@gentasaur.us>
2021-01-08 18:23:28 -0500

    added team code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/USAutoRed.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RobotBrian;
+
+@Autonomous(name = "US Auto Red")
+public class USAutoRed extends OpMode {
+
+    
+
+    @Override
+    public void init() {
+        //initializes timer, starting the autonomous
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomous2021/vuforia.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.autonomous.autonomous2021;
+
+public class vuforia {
+}
