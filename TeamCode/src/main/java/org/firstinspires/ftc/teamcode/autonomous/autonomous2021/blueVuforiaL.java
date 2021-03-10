f51c029a5130783447d6ed2fddfa66e7732bb686
ftc8120 <60890242+ftc8120@users.noreply.github.com>
2021-03-09 20:09:46 -0500

    Update blueVuforiaL.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomous2021/blueVuforiaL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomous2021/blueVuforiaL.java
@@ -12,22 +12,28 @@ import org.firstinspires.ftc.teamcode.RobotBrian;
 
 import java.util.List;
 
-@Autonomous(name = "blueVuforiaL")
-public class blueVuforiaL extends OpMode {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+@Autonomous(name = "redVuforiaR")
+public class redVuforiaR extends OpMode {
 
     ElapsedTime timer;
     private RobotBrian robot;
     private double speed;
     private int state;
 
-    private final int half_block=1000;//actually figure it out
+    private final double half_block=3;//actually figure it out
 
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad"; //4rings
     private static final String LABEL_SECOND_ELEMENT = "Single";//1ring
 
     private static final String VUFORIA_KEY =
-            "AYef6RP/////AAABmQhqgETT3Uq8mNFqAbjPOD990o1n/Osn3oBdTsKI0NXgPuXS612xYfN5Q65srnoMx2eKXe32WnMf6M2BSJSgoPfTZmkmujVujpE/hUrmy5p4L7CALtVoM+TDkfshpKd+LGJT834pEOYUqcUj+vySs3OZQNepaSflmiShfHRNVbrgjrEs1Erlg7zZzc6EQo+yvh0fFtUiQUPLCCcZEPyfnU4k0o8phhbR+Ca9B6dtoeNaYITGHvMmOkBLsyAnR/RQ4Xv8KpvSaSfk0PDyzCG7UsN49k055xOxkFI0iKYp7NMCDF+cezE80dkcnpZCzg1RpGuSpCKGuUbSkJp+q5qudl2qZfWnQntaNI0vlNKD2x1C";
+            "AYef6RP/////AAABmQhqgETT3Uq8mNFqAbjPOD990o1n/Osn3oBdTsKI0NXgPuXS612xYfN5Q65srnoMx2eKX" +
+                    "e32WnMf6M2BSJSgoPfTZmkmujVujpE/hUrmy5p4L7CALtVoM+TDkfshpKd+LGJT834pEOYUqcUj+v" +
+                    "ySs3OZQNepaSflmiShfHRNVbrgjrEs1Erlg7zZzc6EQo+yvh0fFtUiQUPLCCcZEPyfnU4k0o8phhbR" +
+                    "+Ca9B6dtoeNaYITGHvMmOkBLsyAnR/RQ4Xv8KpvSaSfk0PDyzCG7UsN49k055xOxkFI0iKYp7NMCD" +
+                    "F+cezE80dkcnpZCzg1RpGuSpCKGuUbSkJp+q5qudl2qZfWnQntaNI0vlNKD2x1C";
 
     /**
      * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
@@ -99,20 +105,8 @@ public class blueVuforiaL extends OpMode {
     public void loop() {
         switch(state)
         {
+
             case 0:
-                robot.right(speed);
-                if(timer.milliseconds()> half_block ){
-                    next();
-                }
-                break;
-
-            case 1:
-                robot.forward(speed);
-                if(timer.milliseconds()>2*half_block)
-                    next();
-                break;
-
-            case 2:
                 if(tfod != null){
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if(updatedRecognitions != null){
@@ -141,110 +135,59 @@ public class blueVuforiaL extends OpMode {
                 }
                 break;
 
-            case 3: // square A
-                // go to square A
-                robot.left(speed);
+            case 1: // square A
+                robot.right(speed);
                 if(timer.seconds()>1)
                     next();
                 break;
 
-            case 4:
+            case 2:
 
                 robot.forward(speed);
-                if(timer.seconds()>2)
+                if(timer.seconds()>2.75)
                     next();
                 break;
 
-            case 5:
+            case 3:
                 // drop the thing in box using servo
-                robot.left(speed);
+                robot.stop();
+                if(timer.seconds()>3)
+                    next();
+                break;
+
+
+
+            case 103://square c
+                robot.right(speed);
                 if(timer.seconds()>1)
-                    next();
-                break;
-
-            case 6://park on white tape
-                robot.backward(speed);
-                if(timer.seconds()>.5)
-                    next();
-                break;
-
-
-
-
-
-
-
-            case 103://square B
-                //move
-                robot.impell(speed);
-                robot.forward(speed);
-                if(timer.seconds()>half_block)
                     next();
                 break;
 
             case 104:
                 robot.forward(speed);
-                if(timer.seconds()>3)
+                if(timer.seconds()>5)
                     next();
                 break;
 
             case 105:
-                robot.stoppell();
-                robot.forward(speed);
-                if(timer.seconds()>1)
-                    next();
-                break;
-
-            case 106:
-                // drop the thing in box using servo
-                robot.left(speed);
-                if(timer.seconds()>1)
-                    next();
-                break;
-
-            case 107:
                 //park on white tape
                 robot.backward(speed);
-                if(timer.seconds()>2)
+                if(timer.seconds()>1.8)
                     next();
                 break;
 
 
 
-
-
-
-
-            case 203:
-                robot.impell(speed);
+            case 203://square b
                 robot.forward(speed);
-                if(timer.seconds()>half_block)
-                    next();
+                if(timer.seconds()>3.5)
+                next();
                 break;
 
             case 204:
-                robot.stoppell();
-                robot.left(speed);
-                if(timer.seconds()>3)
-                    next();
-                break;
-
-            case 205:
-                robot.forward(speed);
-                if(timer.seconds()>3)
-                    next();
-
-            case 206:
                 // drop the thing in box using servo
-                robot.left(speed);
-                if(timer.seconds()>1)
-                    next();
-                break;
-
-            case 207:
-                //park on white tape
                 robot.backward(speed);
-                if(timer.seconds()>3)
+                if(timer.seconds()>.8)
                     next();
                 break;
         }
